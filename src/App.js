cd5cd969c1efc93d05b3e4cba705722ef8fdf857
Marcin Świderek <marcin.swiderek@htdevelopers.com>
2021-11-25 23:25:14 +0100

    Add animations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,156 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { AnimatePresence, LayoutGroup } from "framer-motion";
+import dragonImg from "./assets/dragon_shadow.png";
+import appleImg from "./assets/apple3_shadow.png";
+import bananaImg from "./assets/bananas_shadow.png";
+import {
+	AppContainer,
+	AppWrapper,
+	TabsContainer,
+	Tab,
+	Underline,
+	TabWrapper,
+	ContentContainer,
+	Image,
+	FruitName,
+	FruitBackground,
+	FruitDescription,
+	FruitInfoContainer,
+	FruitPronouncination,
+} from "./styled";
+
+const fruits = [
+	{
+		id: 0,
+		name: "Dragon fruit",
+		color: "#CC617F",
+		img: dragonImg,
+		pronouncination: "dra · guhn · froot",
+		desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum iste sint architecto iusto odit cupiditate suscipit recusandae, aliquid quod. Consequuntur.",
+	},
+	{
+		id: 1,
+		name: "Green Apple",
+		color: "#8FC32D",
+		img: appleImg,
+		pronouncination: "a · pl",
+		desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum quod modi ratione dicta eaque, porro eius excepturi totam sint facere cumque animi!",
+	},
+	{
+		id: 2,
+		name: "Nana Banana",
+		color: "#ffe135",
+		img: bananaImg,
+		pronouncination: "buh · na · nuh",
+		desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste accusantium, quis ex reprehenderit libero repudiandae soluta nihil ab amet facere unde a libero.",
+	},
+];
+
+const Directions = {
+	Left: "left",
+	Right: "right",
+};
+
+const variants = {
+	initial: (direction) => ({
+		x: direction === Directions.Right ? 300 : -300,
+	}),
+	animate: {
+		x: 0,
+	},
+	exit: (direction) => ({
+		x: direction === Directions.Left ? 300 : -300,
+	}),
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [selectedFruit, setSelectedFruit] = useState(fruits[0]);
+	const [prevSelectedFruit, setPrevSelectedFruit] = useState(fruits[0]);
+	const [direction, setDirection] = useState(Directions.Right);
+
+	useEffect(() => {
+		console.log("direction:", direction);
+	}, [direction]);
+
+	const selectFruit = (fruit) => {
+		setPrevSelectedFruit(selectedFruit);
+		setSelectedFruit(fruit);
+
+		setDirection(
+			fruit.id > selectedFruit.id ? Directions.Right : Directions.Left
+		);
+	};
+
+	const renderTabs = () =>
+		fruits.map((fruit, index) => {
+			const isSelected = selectedFruit.id === fruit.id;
+
+			return (
+				<TabWrapper key={fruit.id}>
+					<Tab
+						bg={fruit.color}
+						onClick={() => selectFruit(fruits[index])}
+						layout
+						animate={{
+							opacity: isSelected ? 1 : 0.4,
+						}}
+						transition={{ duration: 0.3 }}
+					/>
+					{isSelected && (
+						<Underline
+							layoutId="underline"
+							initial={{ background: prevSelectedFruit.color }}
+							animate={{ background: selectedFruit.color }}
+							transition={{ duration: 0.3 }}
+						/>
+					)}
+				</TabWrapper>
+			);
+		});
+
+	return (
+		<AppContainer>
+			<AppWrapper
+				animate={{ borderColor: selectedFruit.color }}
+				transition={{ duration: 0.3 }}
+			>
+				<TabsContainer>
+					<LayoutGroup>{renderTabs()}</LayoutGroup>
+				</TabsContainer>
+				<ContentContainer>
+					<AnimatePresence custom={direction} initial={false}>
+						<Image
+							key={selectedFruit.img}
+							src={selectedFruit.img}
+							alt="Dragon fruit"
+							custom={direction}
+							variants={variants}
+							initial="initial"
+							animate="animate"
+							exit="exit"
+							transition={{ duration: 1 }}
+						/>
+
+						<FruitInfoContainer>
+							<FruitName>{selectedFruit.name}</FruitName>
+							<FruitPronouncination>
+								{selectedFruit.pronouncination}
+							</FruitPronouncination>
+							<FruitDescription>
+								{selectedFruit.desc}
+							</FruitDescription>
+						</FruitInfoContainer>
+					</AnimatePresence>
+
+					<FruitBackground
+						initial={{ background: prevSelectedFruit.color }}
+						animate={{ background: selectedFruit.color }}
+						transition={{ duration: 0.3 }}
+					/>
+				</ContentContainer>
+			</AppWrapper>
+		</AppContainer>
+	);
 }
 
 export default App;
---
+++ src/styled.js
@@ -0,0 +1,117 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+export const AppContainer = styled.div`
+	width: 100vw;
+	height: 100vh;
+	display: grid;
+	place-items: center;
+	background: #e8f7f3;
+`;
+
+export const AppWrapper = styled(motion.div)`
+	width: 340px;
+	height: 700px;
+	background: #e8f7f3;
+	border: solid 20px;
+	border-radius: 50px;
+	box-shadow: rgba(0, 0, 0, 0.1) 0px 20px 25px -5px,
+		rgba(0, 0, 0, 0.04) 0px 10px 10px -5px;
+
+	display: grid;
+	grid-template-columns: 1fr;
+	grid-template-rows: min-content 1fr;
+
+	box-sizing: border-box;
+	padding: 16px;
+
+	overflow: hidden;
+`;
+
+export const TabsContainer = styled.div`
+	width: 100%;
+	display: grid;
+	gap: 16px;
+	grid-template-columns: repeat(3, 1fr);
+`;
+
+export const TabWrapper = styled(motion.div)`
+	display: grid;
+	grid-template-columns: 1fr;
+	grid-template-rows: 1fr min-content;
+	place-items: center;
+	gap: 6px;
+`;
+
+export const Tab = styled(motion.div)`
+	width: 100%;
+	height: 40px;
+	background: ${({ bg }) => bg};
+	border-radius: 12px;
+	cursor: pointer;
+`;
+
+export const Underline = styled(motion.div)`
+	width: 95%;
+	height: 4px;
+	border-radius: 16px;
+`;
+
+export const ContentContainer = styled(motion.div)`
+	position: relative;
+	width: 100%;
+	height: 100%;
+`;
+
+export const Image = styled(motion.img)`
+	position: absolute;
+	bottom: 0;
+	width: 140%;
+	left: -50px;
+
+	z-index: 300;
+`;
+
+export const FruitInfoContainer = styled.div`
+	position: absolute;
+	top: 30px;
+	left: 0;
+`;
+
+export const FruitName = styled.div`
+	font-family: "Merriweather", serif;
+	font-weight: 900;
+	font-size: 64px;
+	line-height: 90%;
+	margin-bottom: 20px;
+	color: #333;
+
+	z-index: 200;
+`;
+
+export const FruitPronouncination = styled.div`
+	font-family: "Merriweather", serif;
+	font-style: italic;
+	font-weight: 700;
+	font-size: 24px;
+	margin-bottom: 20px;
+	color: #333;
+`;
+
+export const FruitDescription = styled.div`
+	font-family: "Karla", sans-serif;
+	font-weight: 400;
+	line-height: 22px;
+	color: #333;
+`;
+
+export const FruitBackground = styled(motion.div)`
+	position: absolute;
+	bottom: -75px;
+	right: -100px;
+	border-radius: 100%;
+	width: 350px;
+	height: 350px;
+	opacity: 0.15;
+	z-index: 100;
+`;
