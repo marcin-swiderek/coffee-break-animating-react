b55340f0c882ddb878bab460148ce1595b4bfbb2
Marcin Świderek <marcin.swiderek@htdevelopers.com>
2021-11-30 09:40:11 +0100

    Resolve conflicts

--- src/App.js
+++ src/App.js
@@ -1,27 +1,3 @@
-<<<<<<< Updated upstream
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-=======
 import React, { useState } from "react";
 import { AnimatePresence } from "framer-motion";
 import dragonImg from "./assets/dragon_shadow.png";
@@ -152,7 +128,6 @@ function App() {
 			</AppWrapper>
 		</AppContainer>
 	);
->>>>>>> Stashed changes
 }
 
 export default App;
